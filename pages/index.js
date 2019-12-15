e1cf41bfda6275679f3585850bb5616a71ed8ef1
oahehc <oahehc@gmail.com>
2019-12-15 09:26:13 +0800

    add index page

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Cookies from "js-cookie";
+import React from "react";
+import Link from "next/link";
 
-export const cookies = ["basic", "expire", "path", "secure"];
-const cookieControl = [
-  {
-    key: "basic",
-    additional: {}
-  },
-  {
-    key: "expire",
-    additional: {
-      expires: 1
-    }
-  },
-  {
-    key: "secure",
-    additional: {
-      secure: true
-    }
-  }
-];
+const Pages = ["basic", "path", "httponly", "samesite"];
 
 const Index = () => {
-  const [cookieData, setCookieData] = useState({});
-
-  function loadCookies() {
-    cookies.forEach(key => {
-      const value = Cookies.get(key) || "";
-
-      setCookieData(data => {
-        return {
-          ...data,
-          [key]: value
-        };
-      });
-    });
-  }
-  function setCookie(key, value, additional) {
-    Cookies.set(key, value, additional);
-    loadCookies();
-  }
-  function removeCookie(key) {
-    Cookies.remove(key);
-    loadCookies();
-  }
-
-  useEffect(() => {
-    loadCookies();
-  }, []);
-
   return (
     <>
-      <div>
-        {cookieControl.map(({ key, additional }) => (
-          <div key={key}>
-            <h3>{key}</h3>
-            <button
-              onClick={() => setCookie(key, `${key} value`, additional)}
-            >{`Set ${key}`}</button>
-            <button onClick={() => removeCookie(key)}>{`Delete ${key}`}</button>
-          </div>
-        ))}
-      </div>
-      <div>
-        <h3>Result</h3>
-        {Object.entries(cookieData).map(([key, value]) => (
-          <div key={key}>{`${key}: ${value}`}</div>
-        ))}
-      </div>
+      {Pages.map(path => (
+        <div key={path}>
+          <Link href={`/${path}`}>
+            <a>{path}</a>
+          </Link>
+        </div>
+      ))}
     </>
   );
 };
